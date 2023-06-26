b4637f78d6aba1b1235ae70a2ea230b42ef19d0b
Denis <den.denis.orlov@bk.ru>
2023-06-26 23:12:42 +0300

    header

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.jpg'
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={"centr"} justify={"space-around"}>
+                <div>
+                    <span>Hi There</span>
+                    <Name>I am Svetlana Dyablo</Name>
+                    <MianTitle>A Web Developer.</MianTitle>
+                </div>
+
+                <Photo src={photo} alt=""/>
+
+            </FlexWrapper>
+
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #fff5e7;
+`
+
+
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`
+
+const MianTitle = styled.h1`
+
+`
+
+
+const Name = styled.h2`
+
+`
