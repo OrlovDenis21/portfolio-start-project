d29a041a933b51944777cd62831b885efe550633
Denis <den.denis.orlov@bk.ru>
2023-07-17 19:13:32 +0300

    header 2

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components'
 import {Logo} from "../../components/logo/Logo";
-import {Menu} from "../../components/menu/Menu";
+
 
 import {FlexWrapper} from "../../components/FlexWrapper";
 import {Container} from "../../components/Container";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
 
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
@@ -15,7 +16,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo/>
-                    <Menu menuItems={items}/>
+                    <HeaderMenu menuItems={items}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
@@ -23,6 +24,11 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
-  background-color: #d4ffd3;
-
+  background: rgba(31,31, 32, 0.9);
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 99999;
 `
