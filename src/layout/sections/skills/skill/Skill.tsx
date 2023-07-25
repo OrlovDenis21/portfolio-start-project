139a573935f770f0f58728f27c49ef2505250d59
Denis <den.denis.orlov@bk.ru>
2023-07-25 20:11:33 +0300

    My Works

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -55,15 +55,9 @@ const IconWrapper = styled.div`
 
 const SkillTitle = styled.h3`
   margin: 70px 0 15px;
-  font-family: 'Josefin Sans', sans-serif;
-  font-weight: 700;
-  font-size: 16px;
-  letter-spacing: 1px;
   text-transform: uppercase;
 `
 const SkillText = styled.p`
   text-align: center;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 1.4;
+
 `
