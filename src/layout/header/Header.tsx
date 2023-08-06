82cfef74e84ab015143a55e2d82dcb15d6c22e65
Denis <den.denis.orlov@bk.ru>
2023-08-06 22:46:41 +0300

    Refactoring

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import styled from 'styled-components'
-import {Logo} from "../../components/logo/Logo";
+import React from "react";
+import { Logo } from "../../components/logo/Logo";
+
+import { FlexWrapper } from "../../components/FlexWrapper";
+import { Container } from "../../components/Container";
+import { DesktopMenu } from "./headerMenu/desktopMenu/DesktopMenu";
+import { MobileMenu } from "./headerMenu/mobileMenu/MobileMenu";
+import { S } from "./Header_Styles";
 
 
-import {FlexWrapper} from "../../components/FlexWrapper";
-import {Container} from "../../components/Container";
-import {HeaderMenu} from "./headerMenu/HeaderMenu";
-import {MobileMenu} from "./mobileMenu/MobileMenu";
+const items = ["Home", "Skills", "Works", "Testimony", "Contact"];
+
+export const Header: React.FC = () => {
+    const [width, setWidth] = React.useState(window.innerWidth);
+    const breakpoint = 768;
+
+    React.useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth)
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
 
 
-const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
+  return (
+    <S.Header>
+      <Container>
+        <FlexWrapper justify={"space-between"} align={"center"}>
+          <Logo />
 
-export const Header = () => {
-    return (
-        <StyledHeader>
-            <Container>
-                <FlexWrapper justify={"space-between"} align={"center"}>
-                    <Logo/>
-                    <HeaderMenu menuItems={items}/>
-                    <MobileMenu menuItems={items}/>
-                </FlexWrapper>
-            </Container>
-        </StyledHeader>
-    );
+            {width < breakpoint ? <MobileMenu menuItems={items} />
+                                :<DesktopMenu menuItems={items} /> }
+
+
+
+        </FlexWrapper>
+      </Container>
+    </S.Header>
+  );
 };
-
-const StyledHeader = styled.header`
-  background: rgba(31,31, 32, 0.9);
-  padding: 20px 0;
-  position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  z-index: 99999;
-`
--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,19 +4,18 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
-import {IconWrapper} from "../skills/skill/Skill";
 import {Container} from "../../../components/Container";
-
-export const Testimony = () => {
+import {S} from '../skills/Skills_Styled'
+export const Testimony: React.FC = () => {
     return (
         <StyledTestimony>
             <Container>
                 <SectionTitle>Testimony</SectionTitle>
                 <FlexWrapper direction={"column"} align={"center"}>
 
-                    <IconWrapper>
+                    <S.IconWrapper>
                         <Icon iconId={"quote"}/>
-                    </IconWrapper>
+                    </S.IconWrapper>
 
                     <Slider/>
                 </FlexWrapper>
@@ -29,7 +28,7 @@ const StyledTestimony = styled.section`
   background-color: #aeb9ff;
   min-height: 50vh;
 
-  ${IconWrapper} {
-    margin: 40px 0 72px;
+  ${S.IconWrapper} {
+    margin: 28px 0 72px;
   }
 `
