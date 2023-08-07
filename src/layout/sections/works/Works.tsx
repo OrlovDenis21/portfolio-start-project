d8c721929d432902d3cbde249382ebf097904c4f
Denis <den.denis.orlov@bk.ru>
2023-08-07 17:49:45 +0300

    Tabs (useState + filter)

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {SectionTitle} from "../../../components/SectionTitle";
-import {TabMenu} from "./tabMenu/TabMenu";
+import {TabMenu, TabsStatusType} from "./tabMenu/TabMenu";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Work} from "./work/Work";
 import socialImg from '../../../assets/images/project-1.png'
@@ -9,14 +9,38 @@ import timerImg from '../../../assets/images/project-2.png'
 import {Container} from '../../../components/Container';
 import {S} from './Works_Styled'
 
-const worksItems = ["All", "landing page", "React", "spa"]
+
+const tabsItems:  Array<{status: TabsStatusType, title: string}> = [
+    {
+        title: "All",
+        status: "all"
+    },
+
+    {
+        title: "landing page",
+        status: "landing"
+    },
+
+    {
+        title: "React",
+        status: "react"
+    },
+
+    {
+        title: "spa",
+        status: "spa"
+    },
 
 
-const workData = [
+]
+
+
+const worksData = [
     {
         title: "Social Network",
         src: socialImg,
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        type: "spa"
     },
 
 
@@ -24,20 +48,42 @@ const workData = [
         title: "Timer",
         src: timerImg,
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim",
+        type: "react"
     }
 
 ]
 
 
 export const Works: React.FC = () => {
+
+    const [currentFilterStatus, setCurrentFilterStatus] = useState("all")
+    let filteredWorks = worksData
+
+    if (currentFilterStatus === "react") {
+        filteredWorks = worksData.filter(work => work.type === "landing")
+    }
+    if (currentFilterStatus === "landing") {
+        filteredWorks = worksData.filter(work => work.type === "react")
+    }
+    if (currentFilterStatus === "spa") {
+        filteredWorks = worksData.filter(work => work.type === "spa")
+    }
+
+    function changeFilterStatus (value: TabsStatusType) {
+        setCurrentFilterStatus(value)
+    }
+
+
     return (
         <S.Works>
             <Container>
                 <SectionTitle>My Works</SectionTitle>
-                <TabMenu menuItems={worksItems}/>
+                <TabMenu tabsItems={tabsItems}
+                         changeFilterStatus={changeFilterStatus}
+                         currentFilterStatus={currentFilterStatus}/>
                 <FlexWrapper justify={"space-between"} align={"flex-start"} wrap={"wrap"}>
 
-                    {workData.map((w)=>{
+                    {filteredWorks.map((w)=> {
                         return <Work title={w.title}
                                      src={w.src}
                                      text={w.text}/>
